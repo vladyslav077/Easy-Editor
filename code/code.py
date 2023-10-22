c49624dd81be2527dd65385df182e357b337ef56
vadimzhenchur <141917559+vadimzhenchur@users.noreply.github.com>
2023-10-22 18:15:41 +0300

    1 type

--- code/code.py
+++ code/code.py
@@ -1,1 +1,40 @@
+from PyQt5.QtWidgets import *
 
+app = QApplication([])
+window = QWidget()
+window.resize(800, 550)
+
+
+
+menuBth = QPushButton("Вліво")
+restBth = QPushButton("Вправо")
+rektorBth = QPushButton("Різкість")
+tagsBth = QPushButton("Ч/Б")
+tags2Bth = QPushButton("Папка")
+fara = QListWidget()
+
+
+mainLine = QHBoxLayout()
+qLine = QHBoxLayout()
+mainLine.addLayout(qLine)
+
+firstLine = QVBoxLayout()
+firstLine.addWidget(tags2Bth)
+firstLine.addWidget(fara)
+
+
+
+mainLine.addLayout(firstLine)
+
+secondline = QVBoxLayout()
+
+
+
+v2 = QVBoxLayout()
+
+
+
+
+window.setLayout(mainLine)
+window.show()
+app.exec()
