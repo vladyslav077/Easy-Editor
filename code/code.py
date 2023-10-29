594815754525aec45cf7c25f07ae68d695fc9dd3
vadimzhenchur <141917559+vadimzhenchur@users.noreply.github.com>
2023-10-29 17:10:20 +0200

    dds1

--- code/code.py
+++ code/code.py
@@ -2,39 +2,41 @@ from PyQt5.QtWidgets import *
 
 app = QApplication([])
 window = QWidget()
-window.resize(800, 550)
-
-
-
-menuBth = QPushButton("Вліво")
-restBth = QPushButton("Вправо")
-rektorBth = QPushButton("Різкість")
-tagsBth = QPushButton("Ч/Б")
-tags2Bth = QPushButton("Папка")
-fara = QListWidget()
-
-
+window.resize(800, 500)
 mainLine = QHBoxLayout()
-qLine = QHBoxLayout()
-mainLine.addLayout(qLine)
+Non = QHBoxLayout()
+windows1 = QLabel("ПАПКА")
+windows2 = QLabel("АБОНЕНТ")
+windows3 = QLabel("НОМЕР")
+windows4 = QLabel("РІДНИЙ")
+windows5 = QLabel("КОЛЄГА")
+windows6 = QLabel("БАТЬКІВЩИНА")
+windows7 = QLabel("НОМЕРОГРАМ")
 
-firstLine = QVBoxLayout()
-firstLine.addWidget(tags2Bth)
-firstLine.addWidget(fara)
+mono1 = QPushButton("ПАПКА")
+mono2 = QPushButton("АБОНЕНТ")
+mono3 = QPushButton("НОМЕР")
+mono4 = QPushButton("РІДНИЙ")
+mono5 = QPushButton("КОЛЄГА")
+mono6 = QPushButton("БАТЬКІВЩИНА")
+text = QTextEdit()
 
-
-
-mainLine.addLayout(firstLine)
-
-secondline = QVBoxLayout()
-
-
-
-v2 = QVBoxLayout()
-
-
-
+Mon = QVBoxLayout()
+Mon.addWidget(mono1)
+Mon.addWidget(text)
+mainLine.addLayout(Mon)
+Mon1 = QVBoxLayout()
+Mon1.addWidget(windows7)
+Non = QHBoxLayout()
+Non.addWidget(mono2)
+Non.addWidget(mono3)
+Non.addWidget(mono4)
+Non.addWidget(mono5)
+Non.addWidget(mono6)
+Mon1.addLayout(Non)
+mainLine.addLayout(Mon1)
 
 window.setLayout(mainLine)
+
 window.show()
 app.exec()
